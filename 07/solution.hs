1cbb29af6cbdf227200f64f9ee28a42d66fcb423
Matej Kuna <matej.kuna@websupport.sk>
2020-01-13 07:28:07 +0100

    curry

--- 07/solution.hs
+++ 07/solution.hs
@@ -1,6 +1,13 @@
 all' :: (a -> Bool) -> [a] -> Bool
 all' p xs = and (map (p) xs)
 
+-- 03:
+map_foldr :: (a -> b) -> [a] -> [b]
+map_foldr f xs = foldr (\x xs -> f x : xs) [] xs
+
+filter_foldr :: (a -> Bool) -> [a] -> [a]
+filter_foldr f xs = foldr (\x xs -> if f x then x : xs else xs) [] xs
+
 -- 04:
 dec2int :: [Int] -> Int
 dec2int = foldl (\x y -> x*10+y) 0
